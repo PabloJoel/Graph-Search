15036ba47585b9886a2bc41e4a56f46943edec93
Pablo Joel <48284414+PabloJoel@users.noreply.github.com>
2023-09-11 16:52:56 +0200

    Graph documentation

--- Graph/Graph.py
+++ Graph/Graph.py
@@ -1,11 +1,23 @@
 
 
 class Graph:
+    """
+    Dash is the base class to represent a Graph.
+    """
 
     def __init__(self, data):
+        """
+        This method loads the data that represents the Graph.
+
+        :param pd.DataFrame data: pandas DataFrame containing the graph details.
+        """
         self.data = data
 
     def show(self):
+        """
+        This method prints in the console the current state of the Graph.
+        :return:
+        """
         print(self.data)
 
 
