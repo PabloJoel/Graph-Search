d2577faa05c3dd51dac5240a43d64738663b7f74
Pablo Joel <48284414+PabloJoel@users.noreply.github.com>
2023-09-11 16:46:54 +0200

    DashGraph documentation

--- Graph/tests/DashGraph.py
+++ Graph/tests/DashGraph.py
@@ -1,5 +1,6 @@
 import threading
 import re
+import pandas as pd
 from dash import Dash, html
 import dash_cytoscape as cyto
 
@@ -7,35 +8,62 @@ from Graph.Graph import Graph
 
 #todo: check if there is an option to display the graph avoiding edges touching edges
 #todo: increase arrow size and label size
-#todo: documentation
 
 
-class DashGrap(Graph):
+class DashGraph(Graph):
+    """
+    DashGraph is a subclass of Graph, which has a different visualization than its parent class.
+    DashGraph creates a Dash dashboard, where the graph is going to be displayed and updated.
+    """
 
     def __init__(self, data):
+        """
+        This method loads the data and creates a server running Dash on localhost:8050, which displays the loaded graph.
+
+        :param pd.DataFrame data: pandas DataFrame containing the graph details.
+        """
         super().__init__(data)
-        self.elements = self.generate_dash_graph(self.data)
+        self.dash_graph = self.__generate_dash_graph(self.data)   # Transform the graph dataframe to a Dash visualization
 
         self.app = Dash(__name__)
-        self.update_dash(self.elements)
+        self.__update_dash(self.dash_graph)   # Add the graph to the Dash dashboard
 
-        threading.Thread(target=self.run_dash).start()
+        threading.Thread(target=self.__run_dash).start()  # Run the Dash server on the background
 
-    def generate_dash_graph(self, data):
+    def __generate_dash_graph(self, data):
+        """
+        Internal method that converts a pandas dataframe (representing a Graph) into a list that can be ingested by
+        Dash to create a visualization of the graph.
+
+        :param pd.DataFrame data: pandas DataFrame containing the graph details.
+        :return:
+        """
         self.vertices = set()
-        elements = data.apply(self.create_graph, axis=1)
+        elements = data.apply(self.__create_graph, axis=1)
         elements = [item for sublist in elements for item in sublist]
         return elements
 
-    def create_graph(self, row):
+    def __create_graph(self, row):
+        """
+        Internal method that transforms the data from a single row of the dataframe into a list containing: the source
+        vertex, the target vertex and the edge linking both vertices. Repeated vertices are not duplicated.
+
+        :param pd.Series row: pandas Series containing a row from the data DataFrame.
+        :return:
+        """
         res = list()
+
+        # Source vertex
         if row['source'] not in self.vertices:
             res.append({'data': {'id': row['source'], 'label': row['source']}, 'type': 'node', 'classes': 'unselected'})    # Source vertex
             self.vertices.add(row['source'])
+
+        # Target vertex
         if row['target'] not in self.vertices:
             res.append({'data': {'id': row['target'], 'label': row['target']}, 'type': 'node', 'classes': 'unselected'})    # Target vertex
             self.vertices.add(row['target'])
 
+        # Edge from Source to Target
         pattern = re.compile('weight.*')
         weight_columns = list(filter(pattern.match, row.keys()))
         weight = '(' + ','.join([str(row[weight]) for weight in weight_columns]) + ')'
@@ -43,7 +71,13 @@ class DashGrap(Graph):
 
         return res
 
-    def update_dash(self, elements):
+    def __update_dash(self, elements):
+        """
+        Internal method used to update the Dash visualization.
+
+        :param list elements: list of elements to be displayed on the Dash dashboard.
+        :return:
+        """
         self.app.layout = html.Div([
             cyto.Cytoscape(
                 id='cytoscape',
@@ -75,10 +109,19 @@ class DashGrap(Graph):
             )
         ])
 
-    def run_dash(self):
+    def __run_dash(self):
+        """
+        Internal method used to run the Dash server.
+
+        :return:
+        """
         self.app.run(debug=False)
 
     def show(self):
-        self.elements.append({'data': {'source': 'n1', 'target': 'n2', 'weight': 1}, 'type': 'edge'})
-        self.update_dash(self.elements)
+        """
+        This method updates the Dash dashboard to show the current state of the graph.
+        :return:
+        """
+        self.dash_graph = self.__generate_dash_graph(self.data)
+        self.__update_dash(self.dash_graph)
 
