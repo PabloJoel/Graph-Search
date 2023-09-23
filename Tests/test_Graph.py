2e2d21274c126e9255c3850502d22ad784d2f92a
Pablo Joel <48284414+PabloJoel@users.noreply.github.com>
2023-09-23 11:39:44 +0200

    New methodsUpdate to latest graph changes

--- Tests/test_Graph.py
+++ Tests/test_Graph.py
@@ -34,14 +34,14 @@ def test_successors():
 
 def test_add_vertex():
     graph = Graph(pd.DataFrame(), weight_cols=['weight_1','weight_2'])
-    graph.add_vertex(source='s', target='t', weights={'weight_1': 1, 'weight_2': 2})
+    graph.add_edge(source='s', target='t', weights={'weight_1': 1, 'weight_2': 2})
     expected = pd.DataFrame(data=[{'source': 's', 'target': 't', 'weight_1': 1, 'weight_2': 2}])
     assert graph.data.equals(expected)
 
 
 def test_add_vertex_bidirectional():
     graph = Graph(data=pd.DataFrame(), source_col='source', target_col='target', weight_cols=['weight_1', 'weight_2'], bidirectional=True)
-    graph.add_vertex(source='s', target='t', weights={'weight_1': 1, 'weight_2': 2})
+    graph.add_edge(source='s', target='t', weights={'weight_1': 1, 'weight_2': 2})
     expected = pd.DataFrame(data=[
         {'source': 's', 'target': 't', 'weight_1': 1, 'weight_2': 2},
         {'source': 't', 'target': 's', 'weight_1': 1, 'weight_2': 2}
@@ -52,6 +52,44 @@ def test_add_vertex_bidirectional():
 def test_get_weight():
     data = pd.DataFrame()
     graph = Graph(data=data, bidirectional=True, weight_cols=['weight_1','weight_2'])
-    graph.add_vertex(source='s', target='t', weights={'weight_1': 1, 'weight_2': 2})
+    graph.add_edge(source='s', target='t', weights={'weight_1': 1, 'weight_2': 2})
     assert graph.get_weight('s','t') == {'weight_1': 1, 'weight_2': 2}
 
+
+def test_get_weight_empty():
+    data = pd.DataFrame()
+    graph = Graph(data=data, bidirectional=True, weight_cols=['weight_1','weight_2'])
+    graph.add_edge(source='s', target='t', weights={'weight_1': 1, 'weight_2': 2})
+    assert graph.get_weight('s','n1') == dict()
+
+
+def test_get_all_vertices():
+    data = pd.read_csv('graph-data.csv')
+    graph = Graph(data=data)
+    assert graph.get_all_vertices() == {'s', 'n1', 'n2', 'n3', 'n4', 'n5', 'n6', 't'}
+
+
+def test_remove_edge_empty():
+    data = pd.DataFrame()
+    graph = Graph(data=data, weight_cols=['weight_1','weight_2'])
+    graph.add_edge(source='s', target='t', weights={'weight_1': 1, 'weight_2': 2})
+    graph.remove_edge(source='s', target='t')
+    assert graph.data.empty
+
+
+def test_remove_edge_empty2():
+    data = pd.DataFrame()
+    graph = Graph(data=data, weight_cols=['weight_1','weight_2'])
+    graph.remove_edge(source='s', target='t')
+    assert graph.data.empty
+
+
+def test_remove_edge_nonexistant():
+    data = pd.DataFrame()
+    graph = Graph(data=data, weight_cols=['weight_1','weight_2'])
+    graph.add_edge(source='s', target='t', weights={'weight_1': 1, 'weight_2': 2})
+    graph.remove_edge(source='s', target='e')
+    expected = pd.DataFrame(data=[
+        {'source': 's', 'target': 't', 'weight_1': 1, 'weight_2': 2}
+    ])
+    assert graph.data.equals(expected)
